07a6c8f560a6383e9f72d54b38b3104f98417189
miguelcarmona23 <miguelcarmona996@gmail.com>
2020-02-11 12:14:03 -0500

    Added let for offsetX and offsetY

--- JS/script.js
+++ JS/script.js
@@ -4,6 +4,9 @@ const text = document.querySelector(('h1'));
 
 function shadow(e) {
     const { offsetWidth: width, offsetHeight: height } = hero;
+    let { offsetX: x, offsetY: y } = e;
+
+    console.log(x, y);
 }
 
 hero.addEventListener('mousemove', shadow);
