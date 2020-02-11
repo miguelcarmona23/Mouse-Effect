d53b7489601b68dd5a7bdc3f6b4cc3adc858d536
miguelcarmona23 <miguelcarmona996@gmail.com>
2020-02-11 12:12:05 -0500

    Added const offsetWidth and offsetHeight

--- JS/script.js
+++ JS/script.js
@@ -3,7 +3,7 @@ const text = document.querySelector(('h1'));
 
 
 function shadow(e) {
-
+    const { offsetWidth: width, offsetHeight: height } = hero;
 }
 
 hero.addEventListener('mousemove', shadow);
